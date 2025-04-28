9917aa8579252eea8a756ea68c8bfa69a4c43704
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-04-28 14:55:30 +0900

    タスク作成定義

--- ToDo_app/src/1-domain/task/Task.ts
+++ ToDo_app/src/1-domain/task/Task.ts
@@ -1,1 +1,49 @@
 // ドメインモデルを定義する
+
+// タスクのドメインモデルを定義
+export class Task {
+    // 作成日時
+    private task_createdAt: Date;
+    
+    // タスクID
+    private task_id: string;
+
+    // タスクのタイトル
+    private task_title: string;
+
+    // タスクのの内容
+    private task_content: string;
+
+    // タスクのジャンル (例: 仕事, プライベート)進捗度
+    private task_genre: string;
+
+    // タスクの進捗度 (例: 0% - 100%)
+    private task_progress: number;
+    // 期限日時
+    private task_dueDate?: Date;
+
+    // タスク完了ステータス
+    private task_isCompleted: boolean;
+
+    // コンストラクタ
+    constructor(
+        task_id: string,
+        task_title: string,
+        task_content: string,
+        task_genre: string,
+        task_progress: number,
+        task_createdAt: Date,
+        task_dueDate?: Date,
+        task_isCompleted: boolean = false
+    ){
+        this.task_id = task_id;
+        this.task_title = task_title;
+        this.task_content = task_content;
+        this.task_genre = task_genre;
+        this.task_progress = task_progress;
+        this.task_createdAt = task_createdAt;
+        this.task_dueDate = task_dueDate;
+        this.task_isCompleted = task_isCompleted;
+    }
+
+}
