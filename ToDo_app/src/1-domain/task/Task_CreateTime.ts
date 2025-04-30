7564790f645476ff06a1dd40dd9879760f62ef28
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-04-30 10:42:53 +0900

    Task_CreateTime作成

---
+++ ToDo_app/src/1-domain/task/Task_CreateTime.ts
@@ -0,0 +1,14 @@
+export class TaskCreateTime {
+    // 作成日時
+    private task_createdAt: Date;
+
+    // コンストラクタ
+    constructor(task_createdAt: Date) {
+        this.task_createdAt = task_createdAt;
+    }
+
+    // 作成日時を取得するメソッド
+    public getTaskCreateTime(): Date {
+        return this.task_createdAt;
+    }
+}
