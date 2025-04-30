21e7eda24d8c2ea15f8dff4b356a692d05469467
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-04-30 10:58:47 +0900

    Task_Genreファイル作成

---
+++ ToDo_app/src/1-domain/task/Task_Genre.ts
@@ -0,0 +1,19 @@
+export class TaskGenre{
+    // タスクのジャンル (例: 仕事, プライベート)
+    private task_genre: string;
+
+    // コンストラクタ
+    constructor(task_genre: string) {
+        this.task_genre = task_genre;
+    }
+
+    // ジャンルを取得するメソッド
+    public getGenre(): string {
+        return this.task_genre;
+    }
+
+    // ジャンルを設定するメソッド
+    public setGenre(task_genre: string): void {
+        this.task_genre = task_genre;
+    }
+}
