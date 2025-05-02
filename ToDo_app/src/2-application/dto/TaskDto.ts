c6e39512cc8d54b556b0783ad29ef39b1cb73094
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-05-02 13:47:57 +0900

    コメント追記

--- ToDo_app/src/2-application/dto/TaskDto.ts
+++ ToDo_app/src/2-application/dto/TaskDto.ts
@@ -3,12 +3,12 @@
 
 // データ転送オブジェクト（DTO）
 export interface TaskDto {
-    taskID: string;
-    taskName: string;
+    taskID: string; // タスクID
+    taskName: string;   // タスク名
     taskContent: string | undefined; // undefined許容
-    taskGenre: string;
-    taskProgress: number; // 0-100の範囲
-    taskDueDate: string; // YYYY-MM-DD形式の文字列
+    taskGenre: string;  // タスクジャンル
+    taskProgress: number; // 0-100の範囲    // タスク進捗度
+    taskDueDate: string; // YYYY-MM-DD形式の文字列  // タスクの期限
     taskFinishFlag: boolean; // 完了フラグ
-    taskCreateTime: string; // YYYY-MM-DDTHH:mm:ss.sssZ形式の文字列
+    taskCreateTime: string; // YYYY-MM-DDTHH:mm:ss.sssZ形式の文字列 // タスク作成時間
 }
