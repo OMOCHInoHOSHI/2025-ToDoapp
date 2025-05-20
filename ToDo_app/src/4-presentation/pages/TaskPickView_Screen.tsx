1f4807e2e307a83093255d69781bc3c0e36d66dc
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-05-20 22:52:52 +0900

    タスク個別表示画面のルーティング

--- ToDo_app/src/4-presentation/pages/TaskPickView_Screen.tsx
+++ ToDo_app/src/4-presentation/pages/TaskPickView_Screen.tsx
@@ -4,11 +4,17 @@ import React, { useEffect } from 'react';
 import { useTask } from '../hooks/useTask';
 import TaskButton from '../components/Task_Button';
 import TaskCard from '../components/Task_Card';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import WordScreen from './Resource/Words_Screen.json';
+// import WordTask from './Resource/Words_task.json';
 
 export const TaskPickScreen: React.FC = () => {
     const { tasks, fetchTasks } = useTask();
     const navigate = useNavigate();
+
+    // タスク一覧画面からの遷移時に渡されたタスクIDを取得
+    const location = useLocation();
+    const taskID = location.state?.taskID;
 
     useEffect(() => {
         fetchTasks();
@@ -16,37 +22,24 @@ export const TaskPickScreen: React.FC = () => {
 
     // タスク一覧画面に遷移
     const nav_TaskLitView = () => {
+        console.log('タスク一覧画面に遷移');
         navigate('/tasklistscreen');
     };
 
+    // タスク編集画面に遷移
+    const nav_EditTask = () => {
+        console.log('タスク編集画面に遷移');
+        navigate('/edittaskscreen', {
+            state: {
+                taskID: taskID
+            }
+        });
+    }
+
     return (
         <div>
-            <h1>タスク選択画面</h1>
-            <p>
-                {/* 編集・削除ボタン */}
-                <TaskButton
-                    label="タスク一覧に戻る"
-                    width='150px'
-                    height='50px'
-                    onClick={nav_TaskLitView}
-                />
-            </p>
+            <h1>{WordScreen.TaskPick}{WordScreen.Screen}</h1>
 
-            {tasks.length === 0 ? (
-                <h3>タスクがありません</h3>
-            ) : (
-                <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
-                    {tasks.map((task) => (
-                        <TaskCard
-                            key={task.taskID}
-                            taskName={task.taskName}
-                            taskCreatetime={task.taskCreateTime}
-                            taskProgress={task.taskProgress}
-                            onClick={() => console.log(`タスクID: ${task.taskID}がクリックされました`)}
-                        />
-                    ))}
-                </div>
-            )}
         </div>
     );
 }
