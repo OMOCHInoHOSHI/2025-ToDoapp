3c05b38df9e39676bbdf5ba88b816e452bd3dfcf
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-05-20 12:12:31 +0900

    タスクカードの型定義

---
+++ ToDo_app/src/4-presentation/shared/CardProps.ts
@@ -0,0 +1,9 @@
+// タスクカードの型定義
+
+export type Props_Card = {
+    taskID: string; // タスクID
+    taskName: string; // タスク名
+    taskCreatetime: string; // タスクの作成時間
+    taskProgress: number; // タスクの進捗
+    onClick: () => void; // カードがクリックされたときの処理
+}
