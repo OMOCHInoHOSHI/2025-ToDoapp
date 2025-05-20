1407b8269c445dc6f82d87a2490167c4aa18c47b
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-05-20 15:20:49 +0900

    タスクない場合の処理

--- ToDo_app/src/4-presentation/pages/TaskListView_Screnn.tsx
+++ ToDo_app/src/4-presentation/pages/TaskListView_Screnn.tsx
@@ -2,14 +2,13 @@
 
 import React, { useEffect } from 'react';
 import { useTask } from '../hooks/useTask';
-import TaskButton from '../components/Task_Button';
+// import TaskButton from '../components/Task_Button';
 import TaskCard from '../components/Task_Card';
 
 // タスク一覧画面
 export const TaskListView: React.FC = () => {
-    const { tasks, fetchTasks } = useTask(); // useTaskフックを使用してタスク一覧を取得
+    const { tasks, fetchTasks } = useTask();
 
-    // タスク一覧を取得するためのuseEffectフック
     useEffect(() => {
         fetchTasks();
     }, [fetchTasks]);
@@ -17,18 +16,21 @@ export const TaskListView: React.FC = () => {
     return (
         <div>
             <h1>タスク一覧</h1>
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
-                {tasks.map((task) => (
-                    <TaskCard
-                        key={task.taskID}
-                        taskName={task.taskName}
-                        taskCreatetime={task.taskCreateTime}
-                        taskProgress={task.taskProgress}
-                        onClick={() => console.log(`タスクID: ${task.taskID}がクリックされました`)}
-                    />
-                ))}
-            </div>
-            <TaskButton label="タスク作成" onClick={() => console.log('タスク作成ボタンがクリックされました')} />
+            {tasks.length === 0 ? (
+                <h3>タスクはありません</h3>
+            ) : (
+                <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
+                    {tasks.map((task) => (
+                        <TaskCard
+                            key={task.taskID}
+                            taskName={task.taskName}
+                            taskCreatetime={task.taskCreateTime}
+                            taskProgress={task.taskProgress}
+                            onClick={() => console.log(`タスクID: ${task.taskID}がクリックされました`)}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
